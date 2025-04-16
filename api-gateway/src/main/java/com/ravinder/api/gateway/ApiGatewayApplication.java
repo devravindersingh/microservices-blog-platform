098d80e96bb7f442c52108e429acd4e002acd6c2
Ravinder Singh <ravindersngh402@outlook.com>
2025-04-16 22:11:20 +0530

    readiness of API gateway: #5

--- api-gateway/src/main/java/com/ravinder/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ravinder/api/gateway/ApiGatewayApplication.java
@@ -1,7 +1,12 @@
 package com.ravinder.api.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
