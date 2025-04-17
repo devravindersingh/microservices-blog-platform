8d20ae224078ebf0ab5c3a77de1b8f87971d4b92
Ravinder Singh <ravindersngh402@outlook.com>
2025-04-17 11:35:19 +0530

    Contributes to #4

--- user-service/src/main/java/com/ravinder/api/user/entity/User.java
+++ user-service/src/main/java/com/ravinder/api/user/entity/User.java
@@ -1,19 +1,19 @@
 package com.ravinder.api.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "app_user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
     private int postCount;
+
+    public User() {
+    }
 
     public User(String username) {
         this.username = username;
